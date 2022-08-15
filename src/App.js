48f9d39e2c4c88b39032b5fc79e3089a83ba99ae
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-08-15 11:58:07 +0600

    data fetch from api is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
 import './App.css';
+import React, { useEffect, useState } from "react"
 
 function App() {
+
+  const URL = "https://rest-api-without-db.herokuapp.com/users/";
+
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getAllUsers = () => {
+
+    fetch(URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch")
+        }
+        return res.json();
+      })
+
+      .then((data) => {
+        // console.log(data.users);
+        setUsers(data.users);
+      })
+      .catch((err) => {
+        setError(err.message);
+
+      })
+
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+  };
+
+
+  useEffect(() => {
+
+    getAllUsers();
+
+  }, [])
+
+
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='App'>
+      <h1>Users Management App</h1>
+
+      {isLoading && <h2>Loading .....</h2>}
+      {error && <h2>{error} </h2>}
+
+      <section>
+
+        {users &&
+          users.map((user) => {
+            const { id, username, email } = user;
+            return (
+              <article key={id} className='card'>
+                <p> {username} </p>
+                <p> {email} </p>
+                <button className='btn'>Edit</button>
+                <button className='btn'>Delete</button>
+
+              </article>
+            )
+          })}
+
+      </section>
+
+
+
 
     </div>
   );
